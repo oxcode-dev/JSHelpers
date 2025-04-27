a8e6a61bb10f85d5b368e9f3ef94950576bc53fd
oxcode <mrexcelsam1@live.com>
2025-04-27 21:28:20 +0100

    pre installation commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import { capitalize, dehyphenate } from './helper'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,7 +12,7 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello Vite ${dehyphenate('good-to-have-you')}!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
