a8b0c17f5150af37f8f96bd97ac8e81ff3e5499c
oxcode <mrexcelsam1@live.com>
2025-04-28 21:39:52 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { capitalize, dehyphenate } from './helper'
+import { capitalize, dehyphenate, slugify } from './helper'
 
 let app = document.getElementById('app')
+let slugString = slugify('Hello world, I am the best man at your wedding last year')
 document.getElementById('capitalize').innerHTML = capitalize('hello World')
-document.getElementById('dehyphenate').innerHTML = capitalize('hello World')
+document.getElementById('slugify').innerHTML = slugify(slugString)
+document.getElementById('dehyphenate').innerHTML = dehyphenate(slugString)
 
