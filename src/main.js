bd2eaa5d11d40904feb0b080b603c822990f5fd2
oxcode <mrexcelsam1@live.com>
2025-04-29 21:03:03 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { capitalize, dehyphenate, slugify, numberFormat } from './helper'
+import { capitalize, dehyphenate, slugify, numberFormat, ucwords } from './helper'
 
 let app = document.getElementById('app')
 let slugString = slugify('Hello world, I am the best man at your wedding last year')
 document.getElementById('capitalize').innerHTML = capitalize('hello World')
 document.getElementById('slugify').innerHTML = slugify(slugString)
 document.getElementById('dehyphenate').innerHTML = dehyphenate(slugString)
+document.getElementById('ucwords').innerHTML = ucwords(slugString)
 document.getElementById('numberFormat').innerHTML = numberFormat(1234567890)
 
