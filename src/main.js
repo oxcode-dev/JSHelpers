a653eb2a076aead704edb736a3e104d39efd6f97
oxcode <mrexcelsam1@live.com>
2025-04-29 21:06:13 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { capitalize, dehyphenate, slugify, numberFormat, ucwords } from './helper'
 
 let app = document.getElementById('app')
-let slugString = slugify('Hello world, I am the best man at your wedding last year')
+let string = 'Hello world, I am the best man at your wedding last year'
+let slugString = slugify(string)
 document.getElementById('capitalize').innerHTML = capitalize('hello World')
 document.getElementById('slugify').innerHTML = slugify(slugString)
 document.getElementById('dehyphenate').innerHTML = dehyphenate(slugString)
-document.getElementById('ucwords').innerHTML = ucwords(slugString)
+document.getElementById('ucwords').innerHTML = ucwords(string)
 document.getElementById('numberFormat').innerHTML = numberFormat(1234567890)
 
