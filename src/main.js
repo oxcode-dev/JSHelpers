0888de6da5b325bf7c65aef79e2ab9d52db80f55
oxcode <mrexcelsam1@live.com>
2025-04-29 21:20:24 +0100

    setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { capitalize, dehyphenate, slugify, numberFormat, ucwords } from './helper'
+import { capitalize, dehyphenate, slugify, numberFormat, ucwords, prettierUrl } from './helper'
 
 let app = document.getElementById('app')
 let string = 'Hello world, I am the best man at your wedding last year'
@@ -7,5 +7,5 @@ document.getElementById('capitalize').innerHTML = capitalize('hello World')
 document.getElementById('slugify').innerHTML = slugify(slugString)
 document.getElementById('dehyphenate').innerHTML = dehyphenate(slugString)
 document.getElementById('ucwords').innerHTML = ucwords(string)
-document.getElementById('prettierUrl').innerHTML = prettierUrl(string)
+document.getElementById('prettierUrl').innerHTML = prettierUrl('http://www.facebook.com')
 document.getElementById('numberFormat').innerHTML = numberFormat(1234567890)
