c9984b1e4832643e587dcd359d669a10be5b77a7
oxcode <mrexcelsam1@live.com>
2025-05-01 14:58:48 +0100

    setup

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -4,12 +4,4 @@ describe('max', () => {
     it('should return  the first argument if it is greater', () => {
         expect(max(2, 1)).toBe(2)
     })
-
-    it('should return  the second argument if it is greater', () => {
-        expect(max(1, 2)).toBe(2)
-    })
-
-    it('should return  the first argument if arguments are equal', () => {
-        expect(max(2, 2)).toBe(2)
-    })
 })  
